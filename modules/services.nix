559d4d38a6509b024b438bfbf8913bf3694aa8dc
W1ngD1nGa5ter <wangyiben1@outlook.com>
2026-04-15 19:42:18 +0800

    Add networking firewall config

--- modules/services.nix
+++ modules/services.nix
@@ -27,4 +27,12 @@
   };
 
   security.rtkit.enable = true;
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 25565 ];
+      allowedUDPPorts = [ 25565 ];
+    };
+  };
 }
